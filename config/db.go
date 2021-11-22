050966b0b767ed402d3049a9de78a99591f39599
damarowen <damarowen@gmailcom>
2021-11-22 18:37:45 +0700

    add env

--- config/db.go
+++ config/db.go
@@ -1,11 +1,14 @@
 package config
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"pustaka-api/models"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 type DbConn struct {
@@ -27,8 +30,19 @@ func ConnectDatabase() (data *DbConn, err error){
 
 	dbSource := &DbConn{}
 
-	dsn := "root:root@tcp(127.0.0.1:3306)/pustaka_api?charset=utf8mb4&parseTime=True&loc=Local"
-	dbSource.DbSQL, err = gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		panic("Failed to load env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	dbSource.DbSQL, err  = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
 
 	if err != nil {
 		log.Panic("Failed to connect to database!")
@@ -39,4 +53,14 @@ func ConnectDatabase() (data *DbConn, err error){
 
 
 	return dbSource, err
+}
+
+
+//CloseDatabaseConnection method is closing a connection between your app and your db
+func CloseDatabaseConnection(db *gorm.DB) {
+	dbSQL, err := db.DB()
+	if err != nil {
+		panic("Failed to close connection from database")
+	}
+	dbSQL.Close()
 }
